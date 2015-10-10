4836bc9cc7c16186683c99e3d42dfc569d1ebe47
kuroz <shellpick@gmail.com>
2015-10-10 16:01:12 +0800

    Firefox 上 DOMContentLoaded 时微博的 DOM 可能还不存在

--- weibo_marker.js
+++ weibo_marker.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/henix/userjs/weibo_marker
 // @description You can place a marker on the last newsfeed you have read, so it can be found easily next time. Ctrl-Click on an item to mark it, again to remove the mark.
 // @author      henix
-// @version     20151010.1
+// @version     20151010.2
 // @include     http://weibo.com/*
 // @include     http://www.weibo.com/*
 // @license     MIT License
@@ -146,20 +146,30 @@ var CSS = (
 '}'
 );
 
-var feedlist = document.querySelector('div.WB_feed');
-if (feedlist) {
-  GM_addStyle(CSS);
-  feedlist.addEventListener('click', clickHandler);
-  var oldId = localStorage.getItem('feedmarkid');
-  if (oldId) {
-    var markOld = function() {
-      var item = Array.prototype.slice.call(feedlist.querySelectorAll("div.WB_feed_type")).find(function(e) { return e.getAttribute("mid") == oldId; });
-      if (item) {
-        item.classList.add("feedmarker-old");
-      } else {
-        setTimeout(markOld, 1000 * 2); // retry after 2s
-      }
-    };
-    markOld();
+function whenExists(query, f) {
+  var x = query();
+  if (x) {
+    f(x);
+  } else {
+    setTimeout(function() { whenExists(query, f); }, 500);
   }
+}
+
+if (document.querySelector("div.WB_miniblog")) {
+  GM_addStyle(CSS);
+  whenExists(function() { return document.querySelector("div.WB_feed"); }, function(feedlist) {
+    feedlist.addEventListener('click', clickHandler);
+    var oldId = localStorage.getItem('feedmarkid');
+    if (oldId) {
+      var markOld = function() {
+        var item = Array.prototype.slice.call(feedlist.querySelectorAll("div.WB_feed_type")).find(function(e) { return e.getAttribute("mid") == oldId; });
+        if (item) {
+          item.classList.add("feedmarker-old");
+        } else {
+          setTimeout(markOld, 1000 * 2); // retry after 2s
+        }
+      };
+      markOld();
+    }
+  });
 }
